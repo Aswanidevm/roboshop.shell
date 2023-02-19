535b09467eef043b9356d2d76432385abbde6770
Aswanidevm <116585233+Aswanidevm@users.noreply.github.com>
2023-02-19 09:07:54 +0300

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,13 @@
-echo -e "\e[32m installing nginx..."
+echo -e "\e[32m installing nginx...\e[0m"
 yum install nginx -y > /tmp/nginxinstall
 
 rm -rf /usr/share/nginx/html/* 
-echo -e "\e[33m extracting roboshop frontend.."
+echo -e "\e[33m extracting roboshop frontend..\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip > /tmp/extraction 
 cd /usr/share/nginx/html 
-echo -e "\e[34m unziping files... \e[0m"
+echo -e "\e[41m unziping files... \e[0m"
 unzip /tmp/frontend.zip > /tmp/unziping
+echo -e "\e[42m enabling nginx... \e[0m"
 systemctl enable nginx 
+echo -e "\e[31m starting nginx... \e[0m"
 systemctl start nginx 
