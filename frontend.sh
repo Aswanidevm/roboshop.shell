aca7cfb0994e19ea8e19f3ef6c573c2ba16662a6
Aswanidevm <116585233+Aswanidevm@users.noreply.github.com>
2023-02-19 09:46:23 +0300

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -11,3 +11,7 @@ echo -e "\e[42m enabling nginx... \e[0m"
 systemctl enable nginx 
 echo -e "\e[31m starting nginx... \e[0m"
 systemctl start nginx 
+echo -e "\e[35m copying roboshop config... \e[0m"
+cp /config/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e "\e[32m restarting nginx... \e[0m"
+systemctl restart nginx 
