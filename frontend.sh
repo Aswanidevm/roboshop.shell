0c4ea3b818722e8e8b032eee5133cd5b84cdd65f
Aswanidevm <maswanidev@gmail.com>
2023-02-19 11:03:51 +0530

    coloring

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,11 @@
-yum install nginx -y
+echo -e "\e[32m installing nginx..."
+yum install nginx -y > /tmp/nginxinstall
+
 rm -rf /usr/share/nginx/html/* 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+echo -e "\e[33m extracting roboshop frontend.."
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip > /tmp/extraction 
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+echo -e "\e[34m unziping files... \e[0m"
+unzip /tmp/frontend.zip > /tmp/unziping
 systemctl enable nginx 
 systemctl start nginx 
