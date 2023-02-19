9d2fbc467aa2aaccc33c9f062631595f1142dad7
Aswanidevm <116585233+Aswanidevm@users.noreply.github.com>
2023-02-19 12:48:46 +0300

    Update catalogue.sh

--- catalogue.sh
+++ catalogue.sh
@@ -8,6 +8,7 @@ cd /app
 unzip /tmp/catalogue.zip
 cd /app 
 npm install 
+cp /config/catalogue.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl enable catalogue 
 systemctl start catalogue
