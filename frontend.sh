8974ab45e6751df8e9fe98cb9e10c306d9a2430a
Aswanidevm <116585233+Aswanidevm@users.noreply.github.com>
2023-02-21 11:06:04 +0300

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -6,20 +6,25 @@ status $?
 
 print_32 "Removing Old Content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
-
+status $?
 
 print_33 "Downloading Frontend Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+status $?
 
 print_34 "Extracting Downloaded Frontend"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
+status $?
 
 print_35 "Copying Nginx Config for RoboShop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
+status $?
 
 print_36 "Enabling nginx"
 systemctl enable nginx &>>${log_file}
+status $?
 
 print_36 "Starting nginx"
 systemctl restart nginx &>>${log_file}
+status $?
