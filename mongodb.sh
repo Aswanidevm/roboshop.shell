cc30efa83ec8ad4a3374d9861b09014808904bb7
Aswanidevm <116585233+Aswanidevm@users.noreply.github.com>
2023-02-22 08:48:46 +0300

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,18 +1,23 @@
 source common.sh
 
-print_head "Setup MongoDB repository"
-cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+print_31 "Setup MongoDB repository"
+cp ${code_dir}/config/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+status $?
 
-print_head "Install MongoDB"
+print_32 "Install MongoDB"
 yum install mongodb-org -y &>>${log_file}
+status $?
 
-print_head "Update MongoDB Listen address"
+print_33 "Update MongoDB Listen address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
+status $?
 
-print_head "Enable MongoDB"
+print_34 "Enable MongoDB"
 systemctl enable mongod &>>${log_file}
+status $?
 
-print_head "Start MongoDB Service"
+print_35 "Start MongoDB Service"
 systemctl restart mongod &>>${log_file}
+status $?
 
 
