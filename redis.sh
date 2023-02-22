1b3ed79537504da14677ed61c9e22b787665c5d4
Aswanidevm <maswanidev@gmail.com>
2023-02-22 15:28:17 +0300

    initial prog

--- redis.sh
+++ redis.sh
@@ -1,19 +1,26 @@
 source common.sh
 
 print_31 " install repo for redis"
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log_file}
+status $?
 
 print_32 "enabling redis 6.2 vesrsion"
-dnf module enable redis:remi-6.2 -y
+dnf module enable redis:remi-6.2 -y &>>${log_file}
+status $?
 
 print_33 "install redis"
-yum install redis -y
+yum install redis -y &>>${log_file}
+status $?
 
 print_34 "port address change"
-sed -i -n "s/127.0.0.1/0.0.0.0/"
+sed -i -n "s/127.0.0.1/0.0.0.0/" /etc/redis.conf &>>${log_file}
+sed -i -n "s/127.0.0.1/0.0.0.0/" /etc/redis/redis.conf &>>${log_file}
+status $?
 
 print_35 "enabling redis"
-systemctl enable redis
+systemctl enable redis &>>${log_file}
+status $?
 
 print_36 "start redis"
-systemctl start redis
+systemctl start redis &>>${log_file}
+status $?
